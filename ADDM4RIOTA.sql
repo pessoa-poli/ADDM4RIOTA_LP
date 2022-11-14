f870ff44f55355bc1dd1e30ccbd689433bbe6fd0
luis.pessoa <luis.pessoa>
2022-11-14 11:37:11 -0300

    Updated DDL creation script  Changes to be committed: 	modified:   ADDM4RIOTA.sql

--- ADDM4RIOTA.sql
+++ ADDM4RIOTA.sql
@@ -25,7 +25,7 @@ CREATE TABLE `ADDM4RIOTA`.`resilient_solution` (
 );
 
 CREATE TABLE `ADDM4RIOTA`.`BRIDGE_iot_critical_object_threat_type` (
-  `ICO_id` varchar(255),
+  `ICO_acronym` varchar(255),
   `TT_id` varchar(255),
   PRIMARY KEY (`ICO_id`, `TT_id`)
 );
@@ -36,10 +36,23 @@ CREATE TABLE `ADDM4RIOTA`.`BRIDGE_resilient_solution_threat_type` (
   PRIMARY KEY (`RESSOL_id`, `TT_id`)
 );
 
-ALTER TABLE `ADDM4RIOTA`.`BRIDGE_iot_critical_object_threat_type` ADD FOREIGN KEY (`ICO_id`) REFERENCES `ADDM4RIOTA`.`iot_critical_object` (`acronym`);
+CREATE TABLE ADDM4RIOTA.CATEGORICAL_threat_type {
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `Name` VARCHAR(255),
+    `Acronym` VARCHAR(20),
+    `Description` VARCHAR(255)
+    };
+
+ALTER TABLE `ADDM4RIOTA`.`BRIDGE_iot_critical_object_threat_type` ADD FOREIGN KEY (ICO_acronym) REFERENCES `ADDM4RIOTA`.`iot_critical_object` (`acronym`);
 
 ALTER TABLE `ADDM4RIOTA`.`BRIDGE_iot_critical_object_threat_type` ADD FOREIGN KEY (`TT_id`) REFERENCES `ADDM4RIOTA`.`threat_type` (`iot_threat_id`);
 
 ALTER TABLE `ADDM4RIOTA`.`BRIDGE_resilient_solution_threat_type` ADD FOREIGN KEY (`RESSOL_id`) REFERENCES `ADDM4RIOTA`.`resilient_solution` (`resilient_solution_id`);
 
 ALTER TABLE `ADDM4RIOTA`.`BRIDGE_resilient_solution_threat_type` ADD FOREIGN KEY (`TT_id`) REFERENCES `ADDM4RIOTA`.`threat_type` (`iot_threat_id`);
+
+delete from ADDM4RIOTA.threat_type where 1=1;
+delete from ADDM4RIOTA.iot_critical_object where 1=1;
+
+delete from ADDM4RIOTA.BRIDGE_resilient_solution_threat_type where 1=1;
+delete from ADDM4RIOTA.BRIDGE_iot_critical_object_threat_type where 1=1;
